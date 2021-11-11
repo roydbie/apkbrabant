92fd2dd557a0f2aca51a7dcccbe633514401df4a
roydebie <48086834+roydbie@users.noreply.github.com>
2021-11-11 17:32:14 +0100

    Menubar gaat naar agenda?weeknummer={week}

--- server/storage/framework/views/menubar.php
+++ server/storage/framework/views/menubar.php
@@ -35,7 +35,7 @@
                 </div>
                 Overzicht
             </div>
-            <div class="menu-item" onclick="location.href='/agenda';">
+            <div class="menu-item" onclick="location.href='/agenda?weeknummer=<?=date('W')?>';">
                 <div class="menucircle m-auto">
                     <p class="pt-2" style="font-size: 25px;">&#128198;</p>
                 </div>
